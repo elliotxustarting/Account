f7e850634ffe62fa553c4e5ec45a5fa33fe99d70
xuyan <xuyan@beisen.com>
2018-06-19 16:50:53 +0800

    init

---
+++ CommonPermission/Permission.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CommonPermission
+{
+    public class Permission
+    {
+    }
+}
---
+++ CommonUser/User.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CommonUser
+{
+    public class User
+    {
+    }
+}
